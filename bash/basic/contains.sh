c16ca37c7b691957dc6b3f77ee79ba207e1f3506
ShigekuniWork <shigekuni.work@gmail.com>
2026-09-08 06:57:35 +0900

    Add Bash string containment example

---
+++ bash/basic/contains.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+var="8dm7KsjU28B7v621Jls"
+value="ERmFRMVZ0U2paTlJYTkxDZz09Cg"
+
+for i in {1..40}
+do
+        var=$(echo $var | base64)
+
+        #<---- If condition here:
+        if [[ "$var" == *"$value"* ]] && [ ${#var} -gt 113450 ]; 
+        then
+            echo "${var: -20}"
+            break
+        fi
+done
